38d70dbb414161e8d6abb95c1ff868d08d739fed
Erling Jahr <erling@fintlabs.no>
2022-01-28 10:59:36 +0100

    updated version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("Vigo")]
 [assembly: AssemblyProduct("VigoBAS")]
-[assembly: AssemblyCopyright("Copyright © 2017 Vigo")]
+[assembly: AssemblyCopyright("Copyright © 2022 Vigo IKS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -44,8 +44,9 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyInformationalVersion("1.1.0")]
-[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.7.0.*")]
+[assembly: AssemblyFileVersion("1.7.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
